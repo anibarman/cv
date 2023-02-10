0e258dbc04c8ba4e02e5fd0ef043e6f51e1888a0
Animesh Barman <animeshbarman.cse@gmail.com>
2023-02-10 16:36:47 +0530

    Delete decreaseBtn.js

--- decreaseBtn.js
+++
@@ -1,9 +0,0 @@
-const decreaseText = () => {
-    const elements = document.querySelectorAll("body *");
-    elements.forEach(element => {
-    let fontSize = window.getComputedStyle(element, null).getPropertyValue("font-size");
-    fontSize = parseFloat(fontSize) * 0.9;
-    element.style.fontSize = fontSize + "px";
-    });
-    };
-    
