f7fa2539f7dbb13621ed08d77ec6c9797c72bbf5
Animesh Barman <animeshbarman.cse@gmail.com>
2023-02-15 20:08:14 +0530

    Update textsize.js

--- Java Scripts/textsize.js
+++ Java Scripts/textsize.js
@@ -1,6 +1,6 @@
 const increaseTextSize = document.getElementById("increaseTextSize");
 const decreaseTextSize = document.getElementById("decreaseTextSize");
-const textElements = document.querySelectorAll("body *:not(.copyright):not(.last-updated):not(.made-by):not(.name_1):not(.title)");
+const textElements = document.querySelectorAll("body *:not(.copyright):not(.last-updated):not(.made-by):not(.name_1):not(.title):not(.ab)");
 
 let fontSize = 16;
 
